51c73e49c93a48c9746dc8584a04297f961371dd
SaadiaAjbbar <ajbbarsaadia6@gmail.com>
2026-01-05 11:48:57 +0100

    premiere structure de projet

--- app/Views/pages/book_show.php
+++
@@ -1,6 +0,0 @@
-<?php if ($book): ?>
-    <h2><?= htmlspecialchars($book['title']) ?></h2>
-    <p><?= htmlspecialchars($book['description']) ?></p>
-<?php else: ?>
-    <p>Book not found.</p>
-<?php endif; ?>
--- app/Views/pages/home.php
+++
@@ -1,5 +0,0 @@
-<h1><?= isset($title) ? htmlspecialchars($title) : 'Welcome' ?></h1>
-
-<p>This is a mini MVC project demonstration.</p>
-
-<p>Use the navigation above to explore the books page and other sections.</p>
